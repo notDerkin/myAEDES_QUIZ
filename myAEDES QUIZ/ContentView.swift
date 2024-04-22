04c8bacb1b506bbca359c9e3d21f81ab8c5d7115
notDerkin <92524803+notDerkin@users.noreply.github.com>
2024-04-22 15:38:02 +0200

    Add files via upload

---
+++ myAEDES QUIZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myAEDES QUIZ
+//
+//  Created by Raffaele Siciliano on 22/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myAEDES QUIZ/myAEDES_QUIZApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAEDES_QUIZApp.swift
+//  myAEDES QUIZ
+//
+//  Created by Raffaele Siciliano on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct myAEDES_QUIZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
