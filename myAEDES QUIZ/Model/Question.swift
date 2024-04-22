cb9456767aeed5f6e2bd5d9af2bb6d848a804531
Derkin <coverforhobbyit@gmail.com>
2024-04-22 17:04:29 +0200

    aggiunti il colore background e rimosse le avvertenze dei .infinity

---
+++ myAEDES QUIZ/Model/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  myAEDES QUIZ
+//
+//  Created by Raffaele Siciliano on 22/04/24.
+//
+
+import Foundation
---
+++ myAEDES QUIZ/View/Styling.swift
@@ -0,0 +1,8 @@
+//
+//  Styling.swift
+//  myAEDES QUIZ
+//
+//  Created by Raffaele Siciliano on 22/04/24.
+//
+
+import Foundation
